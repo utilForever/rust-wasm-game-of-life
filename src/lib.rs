a9511e263fbc259d3c6ed5998979cc07f81945cc
Chris Ohk <utilForever@gmail.com>
2021-07-21 16:30:03 +0900

    refactor: Delete unnecessary code

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,6 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 #[repr(u8)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
