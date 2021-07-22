edd2b6284dda4e3f9f57e3ec1c1f107d2f9ffe3c
Chris Ohk <utilForever@gmail.com>
2021-07-22 16:41:52 +0900

    feat: Add some setter and getter functions to test

--- src/lib.rs
+++ src/lib.rs
@@ -114,6 +114,36 @@ impl Universe {
     pub fn cells(&self) -> *const u32 {
         self.cells.as_slice().as_ptr()
     }
+
+    /// Set the width of the universe.
+    /// Resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        for i in 0..(self.width * self.height) as usize { self.cells.set(i, false) }
+    }
+
+    /// Set the height of the universe.
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        for i in 0..(self.width * self.height) as usize { self.cells.set(i, false) }
+    }
+}
+
+impl Universe {
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &[u32] {
+        &self.cells.as_slice()
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells.set(idx, true);
+        }
+    }
 }
 
 impl fmt::Display for Universe {
