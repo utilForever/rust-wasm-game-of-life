6edf4badca7eadd5fbd05548fc23d4ae7ef91c65
Chris Ohk <utilForever@gmail.com>
2021-07-26 15:54:31 +0900

    feat: Add struct 'Timer' and code to count it in 'Universe::tick'

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ mod utils;
 
 use std::fmt;
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 use fixedbitset::FixedBitSet;
 
 // A macro to provide 'println!(..)'-style syntax for 'console.log' logging.
@@ -13,6 +14,23 @@ use fixedbitset::FixedBitSet;
 macro_rules! log {
     ( $( $t:tt )* ) => {
         web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
+pub struct Timer<'a> {
+    name: &'a str,
+}
+
+impl<'a> Timer<'a> {
+    pub fn new(name: &'a str) -> Timer<'a> {
+        console::time_with_label(name);
+        Timer { name }
+    }
+}
+
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        console::time_end_with_label(self.name);
     }
 }
 
@@ -70,6 +88,8 @@ impl Universe {
 #[wasm_bindgen]
 impl Universe {
     pub fn tick(&mut self) {
+        let _timer = Timer::new("Universe::tick");
+
         let mut next = self.cells.clone();
 
         for row in 0..self.height {
